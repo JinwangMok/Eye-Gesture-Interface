da36b6c8b1beccb4da29e667829f29f08ba01d03
JinwangMok <wlsdhkd8040@gmail.com>
2022-12-27 15:35:58 +0900

    chore: first make of initialSetUp function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "main.h"
 
 // CASCADE CLASSIFIER
-cv::CascadeClassifier face_cascade;
-cv::CascadeClassifier eye_cascade;
+cv::CascadeClassifier faceClassifier;
+cv::CascadeClassifier eyeClassifier;
 
 int main(int argc, char** argv){
     cv::VideoCapture cap(0);
@@ -11,18 +11,34 @@ int main(int argc, char** argv){
         std::cerr << "Camera load failed!" << std::endl;
         return -1;
     }
-
-    cv::Mat cameraFrame;
     
-    while(true){
+    cv::Mat cameraFrame;
+    const uint16_t fps = cap.get(cv::CAP_PROP_FPS);
 
-        cap >> cameraFrame;
-        
-        cv::imshow("User", cameraFrame);
-        
-        if(cv::waitKey(5)==27){
-            break;
-        }
-    }
+    CURSOR = initialSetUp(cap, cameraFrame, mainWindow, fps);
+    
+    std::cout << CURSOR << std::endl;
+    
+    cap >> cameraFrame;
+    
+    cv::imshow("Main Window", mainWindow);
+    cv::waitKey(0);
     cv::destroyAllWindows();
+}
+
+cv::Point initialSetUp(cv::VideoCapture& cap, cv::Mat& frame, cv::Mat& mainWindow, const uint16_t FPS){
+    /* VARIABLES */
+    const int FRAME_NUM_FOR_INIT = FPS * INIT_SEC;
+
+    //TODO: Add Initial display on the mainWindow like "loading..."
+
+    /* Initialize for INIT_SEC */
+    for(int i = 0; i < FRAME_NUM_FOR_INIT; i++){
+        cap >> frame;
+    }
+
+    //TODO: Update the mainWindow for experiment.
+    MAIN_WINDOW(cv::Size(DISPLAY_W, DISPLAY_H), CV_8UC3, cv::Scalar::all(255));
+    
+    return cv::Point(0, 0);
 }
--- main.h
+++ main.h
@@ -1,7 +1,38 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+/* Includes */
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
+
+/* Constants */
+#define KEY_ESC 27
+#define INIT_SEC 2
+#define CASCADE_FACE_PATH "./haarcascade_frontalface_alt2.xml"
+#define CASCADE_EYE_PATH "./haarcascade_eye.xml"
+// > Display Resolution
+//TODO: Required revise to getting the display resolution.
+#ifdef _WIN32
+#define DISPLAY_W 1280
+#define DISPLAY_H 720
+
+#elif __APPLE__ 
+#define DISPLAY_W 2560
+#define DISPLAY_H 1440
+
+#else
+#define DISPLAY_W 1920
+#define DISPLAY_H 1080
+#endif
+/* Types */
+
+/* Global Variables */
+cv::Point CURSOR;
+cv::Size DISPLAY_SIZE;
+cv::Mat MAIN_WINDOW;
+
+/* Functions */
+cv::Point initialSetUp(cv::VideoCapture& cap, cv::Mat& frame, cv::Mat& mainWindow, const uint16_t FPS);
 
 #endif
